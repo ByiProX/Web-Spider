09ec76e865c0181723a9094d5c6f2be1e20096e4
Quentin Wang <wangkx0105@outlook.com>
2018-02-11 17:50:46 +0800

    spider2 readme.md

--- Spider01/urllib_test02.py
+++ Spider01/urllib_test02.py
@@ -1,7 +1,6 @@
 from urllib import request
 
 if __name__ == "__main__":
-    # response = request.urlopen("http://www.example.com/")
     response = request.urlopen("http://fanyi.baidu.com/")
     html = response.read()
     html = html.decode("utf-8")
