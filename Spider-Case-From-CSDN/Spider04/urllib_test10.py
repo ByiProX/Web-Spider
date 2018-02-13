7051768856221b871948ec60e106b2fb94ed5174
Quentin Wang <wangkx0105@outlook.com>
2018-02-13 16:59:47 +0800

    spider04 done

--- Spider-Case-From-CSDN/Spider04/urllib_test10.py
+++ Spider-Case-From-CSDN/Spider04/urllib_test10.py
@@ -4,15 +4,20 @@ from pprint import pprint
 
 if __name__ == "__main__":
     #访问网址
-    url = 'http://www.whatismyip.com.tw/'
+    url = 'http://ip.chinaz.com/getip.aspx'
+    # url = 'http://www.whatismyip.com.tw/'
+    # url = 'http://www.ip.cn/'
     #这是代理IP
-    proxy = {'http':'106.46.136.112:808'}
+
+    # proxy = {'https':'59.110.221.56:80'}
+    proxy = {'https':'111.155.116.247:8123'}
+
     #创建ProxyHandler
     proxy_support = request.ProxyHandler(proxy)
     #创建Opener
     opener = request.build_opener(proxy_support)
     #添加User Angent
-    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36')]
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')]
     #安装OPener
     request.install_opener(opener)
     #使用自己安装好的Opener
