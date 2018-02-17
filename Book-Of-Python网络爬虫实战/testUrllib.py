9afdc0d14e720fe86a2e69ed873ed86314b1eadd
Quentin Wang <wangkx0105@outlook.com>
2018-02-17 23:23:57 +0800

    add Book-of-python网络爬虫实战

--- Book-Of-Python网络爬虫实战/testUrllib.py
+++ Book-Of-Python网络爬虫实战/testUrllib.py
@@ -3,9 +3,10 @@ from urllib import request
 from urllib import error
 
 def linkBaidu():
-    url = 'https://www.baidu.com'
+    url = 'http://www.baidu.com'
     try:
         response = request.urlopen(url, timeout=1)
+        print(response.read())
     except error.URLError as e:
         print('网络地址错误:', e)
         exit()
