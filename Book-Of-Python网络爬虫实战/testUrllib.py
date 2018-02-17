cb20b34ddf248fb2909010e2afbedd51727ed8da
Quentin Wang <wangkx0105@outlook.com>
2018-02-17 22:23:36 +0800

    add Book-of-python网络爬虫实战

---
+++ Book-Of-Python网络爬虫实战/testUrllib.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+from urllib import request
+from urllib import error
+
+def linkBaidu():
+    url = 'https://www.baidu.com'
+    try:
+        response = request.urlopen(url, timeout=1)
+    except error.URLError as e:
+        print('网络地址错误:', e)
+        exit()
+
+    with open('./baidu.txt', 'w') as fp:
+        fp.write(response.read().decode('utf-8'))
+
+    print(response.geturl())
+    print(response.getcode())
+    print(response.info())
+
+
+if __name__ == '__main__':
+    linkBaidu()
