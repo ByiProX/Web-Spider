999bd504cf1e848844291818efcbdf68556b9272
Quentin Wang <wangkx0105@outlook.com>
2018-02-23 14:19:41 +0800

    多页面爬取方法，starturls in xiciSpider

--- Scrapy-Test/getProxy/getProxy/spiders/xiciSpider.py
+++ Scrapy-Test/getProxy/getProxy/spiders/xiciSpider.py
@@ -6,6 +6,13 @@ from getProxy.items import GetproxyItem
 class XicispiderSpider(scrapy.Spider):
     name = 'xiciSpider'
     allowed_domains = ['xicidaili.com']
-    start_urls = ['http://xicidaili.com/']
+    # start_urls = ['http://xicidaili.com/']
+    kinds = ['nn', 'nt', 'wn', 'wt']
+    pages = 20 #暂时爬取20个页面
+    start_urls =[]
+    for kind in kinds:
+        for i in range(1, pages+1):
+            start_urls.append('http://xicidaili.com/' + kind + '/' + str(i))
+
 
     def parse(self, response):
