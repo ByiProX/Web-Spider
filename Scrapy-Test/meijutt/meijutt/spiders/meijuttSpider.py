c262e0d90a71eecb50bfa2f5cf6501bfd1be0aa3
Quentin Wang <wangkx0105@outlook.com>
2018-02-25 15:46:18 +0800

    cmp meijuttspider.py

--- Scrapy-Test/meijutt/meijutt/spiders/meijuttSpider.py
+++ Scrapy-Test/meijutt/meijutt/spiders/meijuttSpider.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from meijutt.items import MeijuttItem
 
 
 class MeijuttspiderSpider(scrapy.Spider):
     name = 'meijuttSpider'
     allowed_domains = ['meijutt.com']
-    start_urls = ['http://meijutt.com/']
+    start_urls = ['http://www.meijutt.com/new100.html']
 
     def parse(self, response):
-        pass
+        subSelector = response.xpath('//div[@class="top-min top-min-long new100"]')
+
+        items = []
+        for sub in subSelector:
+            item = MeijuttItem()
+
+            item['storyName'] = sub.xpath('.//li/h5//text()').extract()[0]
+            item['storyState'] = sub.xpath('.//span[@class="state1 new100state1"]/text()').extract()[0]
+            item['tvStation'] = sub.xpath('.//span[@class="mjtv"]/text()').extract()[0]
+            item['updateTime'] = sub.xpath('.//span[@class="lasted-time new100time fn-right"]/text()').extract()[0]
+
+            items.append(item)
+        return items
